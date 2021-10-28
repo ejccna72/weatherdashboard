f59901c2f2146f33ffaa45b0ce40c8505dcf0a19
Eric Johnson <ejccna72@outlook.com>
2021-10-28 12:58:41 -0700

    js fetch is finally working and returning basic current conditions for City entered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const api = {
-    key: 8bc4d191da8bb99b847410a7f5c643bc
+    key: "8bc4d191da8bb99b847410a7f5c643bc",
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
@@ -10,3 +10,38 @@ const sTMP = document.querySelector(".tMP");
 const sWTR = document.querySelector(".wTR");
 const shLO = document.querySelector(".hLO");
 const aCRD = document.querySelector(".hDN");
+
+
+function myQuery(e){
+    if(e.keyCode == 13){
+        fetch(`${api.base}weather?q=`+sBOX.value+`&units=imperial&appid=${api.key}`).then(response => response.json()).then(dataDisplay)
+        ;
+    }
+    function dataDisplay(response){
+   
+        if(response.cod === "404"){
+                 
+        aCRD.classList.add('card');
+    
+        sCTY.innerText = 'Invalid City';
+    
+        sTMP.innerText = 'N/A';
+    
+        sWTR.innerText = 'N/A';
+    
+        shLO.innerText = 'N/A' + '/'+ 'N/A';      
+        }else{
+            
+        aCRD.classList.add('card');
+    
+        sCTY.innerText = response['name'] + ','+ response['sys']['country'];
+    
+        sTMP.innerText = response['main']['temp'] + String.fromCharCode(176) + 'c';
+    
+        sWTR.innerText = response.weather[0].main;
+    
+        shLO.innerText = response.main.temp_max + String.fromCharCode(176) + 'c'+ '/'+ response.main.temp_min + String.fromCharCode(176)+ 'c'
+        }
+}
+}
+sBOX.addEventListener('keypress', myQuery);
