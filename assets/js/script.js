746972b7d4d100f0dcc3633afa7eff4a5993c41a
Eric Johnson <ejccna72@outlook.com>
2021-10-28 12:02:41 -0700

    setup base html and div layout for current weather + basic CSS + beginning js variable defs

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const api = {
+    key: 8bc4d191da8bb99b847410a7f5c643bc
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const sBOX = document.querySelector(".sBOX");
+const sCTY = document.querySelector(".cTY");
+const sDTE = document.querySelector(".dTE");
+const sTMP = document.querySelector(".tMP");
+const sWTR = document.querySelector(".wTR");
+const shLO = document.querySelector(".hLO");
+const aCRD = document.querySelector(".hDN");
